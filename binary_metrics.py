0c3113589302df884549decce1b0c89b99e20f62
pbelmann <pbelmann@cebitec.uni-bielefeld.de>
2017-09-19 16:43:50 +0200

    fix(format): return float and fix intendation

--- binary_metrics.py
+++ binary_metrics.py
@@ -15,87 +15,88 @@ Tests can be executed by running
 
 
 def __get_existing_taxa(rank):
-  """ Return set of taxids with abundance > 0
+    """ Return set of taxids with abundance > 0
 
-  >>> __get_existing_taxa(query_rank)
-  [123]
+    >>> __get_existing_taxa(query_rank)
+    [123]
 
-  :param rank: Set of taxids of specific rank
-  :return: list of taxids
-  """
-  return list(k for k, v in rank.iteritems() if v > 0)
+    :param rank: Set of taxids of specific rank
+    :return: list of taxids
+    """
+    return list(k for k, v in rank.items() if v > 0)
 
 
 def __get_non_existing_taxa(rank):
-  """Return set of taxids with abundance <= 0
-  >>> __get_non_existing_taxa(query_rank)
-  [1232]
+    """Return set of taxids with abundance <= 0
+    >>> __get_non_existing_taxa(query_rank)
+    [1232]
 
-  :param rank: Set of taxids of specific rank
-  :return: list of taxids
-  """
+    :param rank: Set of taxids of specific rank
+    :return: list of taxids
+    """
 
-  return list(k for k, v in rank.iteritems() if v <= 0)
+    return list(k for k, v in rank.items() if v <= 0)
 
 
 def __get_tp(rank_query, rank_truth):
-  """ Returns true positive
-  >>> __get_tp(test_query_rank, test_truth_rank)
-  1
+    """ Returns true positive
+    >>> __get_tp(test_query_rank, test_truth_rank)
+    1.0
 
-  """
-  rank_query = __get_existing_taxa(rank_query)
-  rank_truth = __get_existing_taxa(rank_truth)
-  return len(list(set(rank_query).intersection(rank_truth)))
+    """
+    rank_query = __get_existing_taxa(rank_query)
+    rank_truth = __get_existing_taxa(rank_truth)
+    return float(len(list(set(rank_query).intersection(rank_truth))))
 
 
 def __get_fp(rank_query, rank_truth):
-  """ Returns false positive
-  >>> __get_fp(test_query_rank, test_truth_rank)
-  0
+    """ Returns false positive
+    >>> __get_fp(test_query_rank, test_truth_rank)
+    0.0
 
-  """
+    """
 
-  rank_query = __get_non_existing_taxa(rank_query)
-  rank_truth = __get_existing_taxa(rank_truth)
-  return len(list(set(rank_query).intersection(rank_truth)))
+    rank_query = __get_non_existing_taxa(rank_query)
+    rank_truth = __get_existing_taxa(rank_truth)
+    return float(len(list(set(rank_query).intersection(rank_truth))))
 
 
 def __get_fn(rank_query, rank_truth):
     """ Returns false negative
     >>> __get_fn(test_query_rank, test_truth_rank)
-    0
+    0.0
 
     """
     rank_query = __get_existing_taxa(rank_query)
     rank_truth = __get_non_existing_taxa(rank_truth)
-    return len(list(set(rank_query).intersection(rank_truth)))
+    return float(len(list(set(rank_query).intersection(rank_truth))))
 
 
 def precision(tp, fp):
-    return tp/(tp+fp)
+    return tp / (tp + fp)
 
 
 def recall(tp, fn):
-    return tp/(tp+fn)
+    return tp / (tp + fn)
+
 
 def jaccard_index(rank_query, rank_truth):
     """ Returns the Jaccard index
     >>> jaccard_index(test_query_rank, test_truth_rank)
-    1
+    1.0
 
     """
     rank_query = __get_existing_taxa(rank_query)
     rank_truth = __get_existing_taxa(rank_truth)
     intersection = len(list(set(rank_query).intersection(rank_truth)))
     union = len(list(set(rank_query).union(rank_truth)))
-    return intersection/union
+    return intersection / union
 
 
 def compute_rank_metrics(rank_query, rank_truth):
     """ Returns metrics for one rank
     >>> compute_rank_metrics(test_query_rank, test_truth_rank)
-    (1, 1, 1, 0, 0)
+    (1.0, 1.0, 1.0, 0.0, 0.0, 1.0)
 
     """
     tp = __get_tp(rank_query, rank_truth)
@@ -107,12 +108,14 @@ def compute_rank_metrics(rank_query, rank_truth):
 def compute_tree_metrics(query, truth):
     """ Return metrics for tree
     >>> compute_tree_metrics(query_tree, truth_tree)
-    {'species': (1, 1, 1, 0, 0)}
+    {'species': (1.0, 1.0, 1.0, 0.0, 0.0, 1.0)}
     """
     return {rank: compute_rank_metrics(taxids, truth[rank]) for rank, taxids in query.items()}
 
+
 if __name__ == "__main__":
     import doctest
+
     test_query_rank = dict()
     test_query_rank[123] = 0.1
     test_query_rank[1232] = 0.0
