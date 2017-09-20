b65747c5dbdc61e6afc8b8e2ec0af83a4dfce208
Fernando Meyer <fernando.meyer@gmail.com>
2017-09-20 14:19:29 +0200

    add entry point opal.py

--- l1norm.py
+++ l1norm.py
@@ -25,15 +25,29 @@ def print_l1norm(l1norm, stream=sys.stdout):
         stream.write("{}\t{}\n".format(rank, l1norm[rank]))
 
 
+def print_list_l1norm(l1norm_list, labels, stream=sys.stdout):
+    labels_iterator = iter(labels)
+    stream.write("tool\tsuperkingdom\tphylum\tclass\torder\tfamily\tgenus\tspecies\tstrain\n")
+    for l1norm in l1norm_list:
+        stream.write("{}\t".format(next(labels_iterator)))
+        stream.write("\t".join([format(l1norm["superkingdom"], '.3f') if "superkingdom" in l1norm else "na",
+                      format(l1norm["phylum"], '.3f') if "phylum" in l1norm else "na",
+                      format(l1norm["class"], '.3f') if "class" in l1norm else "na",
+                      format(l1norm["order"], '.3f') if "order" in l1norm else "na",
+                      format(l1norm["family"], '.3f') if "family" in l1norm else "na",
+                      format(l1norm["genus"], '.3f') if "genus" in l1norm else "na",
+                      format(l1norm["species"], '.3f') if "species" in l1norm else "na",
+                      format(l1norm["strain"], '.3f') if "strain" in l1norm else "na"]))
+        stream.write("\n")
+
+
 def main():
     parser = argparse.ArgumentParser(description="Compute L1 Norm")
     parser.add_argument('-1', '--file1', help="Profile 1", required=True)
     parser.add_argument('-2', '--file2', help="Profile 2", required=True)
     args = parser.parse_args()
-    file1_path = args.file1
-    file2_path = args.file2
-    f1_rank_to_taxid_to_percentage = load_data.open_profile(file1_path)
-    f2_rank_to_taxid_to_percentage = load_data.open_profile(file2_path)
+    f1_rank_to_taxid_to_percentage = load_data.open_profile(args.file1)
+    f2_rank_to_taxid_to_percentage = load_data.open_profile(args.file2)
     l1norm = compute_l1norm(f1_rank_to_taxid_to_percentage, f2_rank_to_taxid_to_percentage)
     print_l1norm(l1norm)
 
---
+++ opal.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import os
+import errno
+import argparse
+import l1norm as l1
+from utils import load_data
+
+
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+
+def get_labels(labels, profiles_files):
+    if labels:
+        labels_list = [x.strip() for x in labels.split(',')]
+        if len(labels_list) != len(profiles_files):
+            exit('Number of labels does not match the number of files of profiles. Please check parameter -l, --labels.')
+        return labels_list
+    tool_id = []
+    for profile_file in profiles_files:
+        tool_id.append(profile_file.split('/')[-1])
+    return tool_id
+
+
+def evaluate(gold_standard_file, profiles_files, labels, output_dir):
+    # L1 Norm
+    l1norm_list = []
+    gs_rank_to_taxid_to_percentage = load_data.open_profile(gold_standard_file)
+    for profile_file in profiles_files:
+        rank_to_taxid_to_percentage = load_data.open_profile(profile_file)
+        l1norm_list.append(l1.compute_l1norm(gs_rank_to_taxid_to_percentage, rank_to_taxid_to_percentage))
+    f = open(output_dir + "/l1_norm.tsv", 'w')
+    l1.print_list_l1norm(l1norm_list, labels, f)
+    f.close()
+
+    # Other metrics here
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Compute all metrics for one or more profiles")
+    parser.add_argument("-g", "--gold_standard_file", help="Gold standard file", required=True)
+    parser.add_argument("profiles_files", nargs='+', help="Files of profiles")
+    parser.add_argument('-l', '--labels', help="Comma-separated profiles names", required=False)
+    parser.add_argument('-o', '--output_dir', help="Directory to write the results to", required=True)
+    args = parser.parse_args()
+    labels = get_labels(args.labels, args.profiles_files)
+    output_dir = os.path.abspath(args.output_dir)
+    make_sure_path_exists(output_dir)
+    evaluate(args.gold_standard_file, args.profiles_files, labels, output_dir)
+
+
+if __name__ == "__main__":
+    main()
